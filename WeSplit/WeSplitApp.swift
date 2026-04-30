56e3e31d8679df8db2dd0b5e391144f0034204cc
Martin Jensen <martinloesethjensen@gmail.com>
2026-04-30 21:28:27 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Martin Jensen on 30/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
